84d087a11e2121554c17874e8fd3f1530c6e48a7
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2025-03-05 22:08:49 +0300

    add ece metric and test

--- src/evaluation/uncertainty_evaluator.py
+++ src/evaluation/uncertainty_evaluator.py
@@ -1,15 +1,39 @@
+import typing as tp
 from src.evaluation.evaluator_abc import Evaluator, Results
+from abc import ABC
+from src.data_manipulation.custom_dataset_abc import SizedDataset
+from torch import Tensor
 
 
-class UncertaintyEvaluator(Evaluator):
-    def __init__(self, quantiles: list[float]):
+class UncertaintyEvaluator(Evaluator, ABC):
+    def __init__(self, quantiles: list[float]) -> None:
         self.quantiles = quantiles
-    
-    def evaluate(self, model_predictions, test_dataset):
+
+
+class PICP(UncertaintyEvaluator):
+    def evaluate(
+        self,
+        model_predictions: tp.Sequence[Tensor],
+        test_dataset: SizedDataset[Tensor],
+    ) -> Results:
         lbound = model_predictions[:, :, 0]
         ubound = model_predictions[:, :, -1]
         mask = (lbound <= test_dataset) & (test_dataset <= ubound)
         picp = mask.to(float).mean().item()
         metrics = {'picp': picp}
+        res = Results(metrics=metrics, images=None)
+        return res
+
+
+class ECE(UncertaintyEvaluator):
+    def evaluate(self, model_predictions, test_dataset):
+        ece = []
+        for i in range(len(self.quantiles) // 2):    
+            lbound = model_predictions[:, :, i]
+            ubound = model_predictions[:, :, -i-1]
+            mask = (lbound <= test_dataset) & (test_dataset <= ubound)
+            picp = mask.to(float).mean().item()
+            ece.append(abs(picp - (self.quantiles[-i-1] - self.quantiles[i])))
+        metrics = {'ece': sum(ece) / len(ece)}
         res = Results(metrics=metrics, images=None)
         return res
--- src/tests/test_evaluators.py
+++ src/tests/test_evaluators.py
@@ -1,4 +1,4 @@
-from src.evaluation import UncertaintyEvaluator
+from src.evaluation.uncertainty_evaluator import PICP, ECE
 import torch
 
 
@@ -6,15 +6,23 @@ class TestOnSyntheticData:
     def setup_class(self):
         torch.manual_seed(1)
         self.test_dataset = torch.randn(10, 100)
-        pred = torch.zeros(10, 100, 3)
+        pred = torch.zeros(10, 100, 5)
         pred[:, :, 0] = pred[:, :, 0] - 2
-        pred[:, :, 2] = pred[:, :, 2] + 2
+        pred[:, :, 1] = pred[:, :, 1] - 1
+        pred[:, :, 2] = pred[:, :, 2]
+        pred[:, :, 3] = pred[:, :, 3] + 1
+        pred[:, :, 4] = pred[:, :, 4] + 2
         self.model_prediction = pred
-        self.quantiles = [0.025, 0.5, 0.975]
+        self.quantiles = [0.025, 0.16, 0.5, 0.84, 0.975]
     
     def test_picp(self):
-        evaluator = UncertaintyEvaluator(quantiles=self.quantiles)
+        evaluator = PICP(quantiles=self.quantiles)
         res = evaluator.evaluate(self.model_prediction, self.test_dataset)
         assert round(res.metrics['picp'], 2) == (
             self.quantiles[-1] - self.quantiles[0]
         )
+
+    def test_ece(self):
+        evaluator = ECE(quantiles=self.quantiles)
+        res = evaluator.evaluate(self.model_prediction, self.test_dataset)
+        assert round(res.metrics['ece'], 2) == 0
